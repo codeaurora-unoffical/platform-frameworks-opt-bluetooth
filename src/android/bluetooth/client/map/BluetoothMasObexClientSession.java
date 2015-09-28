da0267ce7913a96b71e22c744778492af95e4d20
Nitin Shivpure <nshivpur@codeaurora.org>
2015-09-28 12:43:46 +0530

    Bluetooth: Fix to avoid crash during message listing due to disconnect
    A case, where message listing is going on and user does explicitly
    disconnect. As mInterrupted is not set, so clientThread waits forever
    after completing existing MAS request. Hence setting mInterrupted while
    disconnecting MAS session to solve this crash.
    
    Change-Id: Icdbaf70e917e139bcd2e63544b315fde346f6c2f
    CRs-fixed: 914961

--- src/android/bluetooth/client/map/BluetoothMasObexClientSession.java
+++ src/android/bluetooth/client/map/BluetoothMasObexClientSession.java
@@ -149,6 +149,12 @@ class BluetoothMasObexClientSession {
 
             return true;
         }
+
+        private void shutdown() {
+            Log.w(TAG, "shutdown ");
+            mInterrupted = true;
+            interrupt();
+        }
     }
 
     public BluetoothMasObexClientSession(ObexTransport transport, Handler handler) {
@@ -166,7 +172,7 @@ class BluetoothMasObexClientSession {
 
     public void stop() {
         if (mClientThread != null) {
-            mClientThread.interrupt();
+            mClientThread.shutdown();
 
             (new Thread() {
                 @Override
